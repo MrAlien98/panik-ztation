032b2e401fef8ee93d1ec08ea01cd40195afc109
unknown <themonstermora@gmail.com>
2020-06-19 22:56:14 -0500

    Some style added, problems reading stiling sheets from it's folder so doing it on the html

--- Main.py
+++ Main.py
@@ -7,5 +7,9 @@ app=Flask(__name__)
 def home():
 	return render_template('home.html')
 
+@app.route("/order/")
+def order():
+	return render_template('order.html')
+
 if __name__ == "__main__":
 	app.run(debug=True)
